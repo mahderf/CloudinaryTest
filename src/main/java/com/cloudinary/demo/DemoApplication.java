afde9068f99b2d609cd9260c7476bcbe91d8f26b
mahderf <mymahder@gmail.com>
2017-09-11 17:23:39 -0400

    Testing Cloudinary and email

--- src/main/java/com/cloudinary/demo/DemoApplication.java
+++ src/main/java/com/cloudinary/demo/DemoApplication.java
@@ -1,12 +1,26 @@
 package com.cloudinary.demo;
 
+import com.google.common.collect.Lists;
+import it.ozimov.springboot.mail.configuration.EnableEmailTools;
+import it.ozimov.springboot.mail.model.Email;
+import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
+import it.ozimov.springboot.mail.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.mail.internet.InternetAddress;
+import java.io.UnsupportedEncodingException;
+
+
 @SpringBootApplication
+@EnableEmailTools
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
 }
--- src/main/java/com/cloudinary/demo/controller/HomeController.java
+++ src/main/java/com/cloudinary/demo/controller/HomeController.java
@@ -1,16 +1,23 @@
 package com.cloudinary.demo.controller;
 
+import com.cloudinary.demo.DemoApplication;
 import com.cloudinary.demo.configuration.CloudinaryConfig;
 import com.cloudinary.demo.models.Actor;
 import com.cloudinary.demo.repository.ActorRepository;
 import com.cloudinary.utils.ObjectUtils;
+import com.google.common.collect.Lists;
+import it.ozimov.springboot.mail.model.Email;
+import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
+import it.ozimov.springboot.mail.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.mail.internet.InternetAddress;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 @Controller
@@ -21,10 +28,27 @@ public class HomeController {
     @Autowired
     CloudinaryConfig cloudc;
 
+
+
+    @Autowired
+    public EmailService emailService;
+    public void sendEmailWithoutTemplating() throws UnsupportedEncodingException {
+        final Email email= DefaultEmail.builder()
+                .from(new InternetAddress("mahifentaye@gmail.com", "Marco Tullio Cicero ne"))
+                .to(Lists.newArrayList(new InternetAddress("mymahder@gmail.com","pomponiu s Atticus")))
+                .subject("Laelius de amicitia")
+                .body("Firmamentum autem stabilitatis constantiaeque eius, quam in amicitia q uaermius, fides est.")
+                .encoding("UTF-8").build();
+//		modelObject.put("recipent", recipent);
+        System.out.println("test it");
+        emailService.send(email);
+    }
     @RequestMapping("/")
-    public String listActors(Model model)
-    {
+    public String listActors(Model model) throws UnsupportedEncodingException {
+//        DemoApplication demo=new DemoApplication();
         model.addAttribute("actors", actorRepository.findAll());
+        sendEmailWithoutTemplating();
+//        demo.sendEmailWithoutTemplating("email");
         return "list";
     }
     @GetMapping("/add")
